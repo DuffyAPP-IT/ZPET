e74654b73321b0e63b7549e9ae05434d0db540d5
James Duffy <james@duffy.app>
2020-11-25 16:58:32 +0000

    log message identification

--- ZPET/resources/spider-integration-local.sh
+++ ZPET/resources/spider-integration-local.sh
@@ -5,7 +5,7 @@ echo Build 3
 
 if [ $# -eq 0 ]
 then
-    echo -e "Spider (for ZPETv2) should not be directly executed... Head to DuffyAPP-IT on GitHub to download the standalone distribution!"
+    echo -e "[!] Spider (for ZPETv2) should not be directly executed... Head to DuffyAPP-IT on GitHub to download the standalone distribution!"
     exit 1
 #	echo -e "Usage: ./spider\n-f\t\t\t\tFind Databases & Dump Schemas\n-p\t\t\t\tExtract Apple Photos Shared Album Information\n-i yourpersonalinfo\t\tDetect Personal Information Within Databases\n-a\t\t\t\tExtended Scan"
 fi
@@ -18,15 +18,14 @@ lpngCount=0
 # Check if rootFS is present
 if [ ! -d ../SENSITIVE/private ]
 then
-echo iOS Root Filesystem Not Detected
-echo Exiting For Safety Reasons...
+echo [!] iOS Root Filesystem Not Detected
+echo [!] Exiting For Safety Reasons...
 exit 1
 fi
 
 #Clean Old Directories & Files
 if [ -d ../SENSITIVE/SpiderOUT ]
 then
-echo Old SpiderOUT Directory Found... Removing!
 rm -rf ../SENSITIVE/SpiderOUT
 fi
 
@@ -53,19 +52,19 @@ then
 #		echo Found $dbCount DBs
 
 
-		echo "Extracting Database Schemas (sqlite)"
+		echo "[+] Extracting Database Schemas (sqlite)"
 		find ../SENSITIVE/ -name '*.sqlite' -print -exec ls -lh {} \; -exec sqlite3 {} '.tables' 2>/dev/null \; | tee ../SENSITIVE/SpiderOUT/TableData.txt
 
-		echo "Extracting Database Schemas (db)"
+		echo "[+] Extracting Database Schemas (db)"
 		find ../SENSITIVE/ -name '*.db' -print -exec ls -lh {} \; -exec sqlite3 {} '.tables' 2>/dev/null \; | tee ../SENSITIVE/SpiderOUT/TableData.txt
   
-        echo "Extracting Database Schemas (db)"
+        echo "[+] Extracting Database Schemas (db)"
         find ../SENSITIVE/ -name '*.db' -print -exec ls -lh {} \; -exec sqlite3 {} '.tables' 2>/dev/null \; | tee ../SENSITIVE/SpiderOUT/TableData.txt
         
-        echo "Hidden Database Identification...(this may take a while)"
+        echo "[+] Hidden Database Identification...(this may take a while)"
         find ../SENSITIVE/ -type f -size +10k -size -800k -not -name "*.db" -not -name "*.sqlite*" -exec file '{}' \; 2>/dev/null | grep -H SQLite | cut -f2 -d':' | tee ../SENSITIVE/SpiderOUT/HiddenDBs.txt
         
-        echo "Extracting Hidden Database Schemas (db)"
+        echo "[+] Extracting Hidden Database Schemas (db)"
         count=0
         while read p; do
         sqlite3 "$p" '.tables' > ../SENSITIVE/SpiderOUT/HiddenTableData.txt
@@ -77,10 +76,10 @@ fi
 
 if [[ $1 = "-p" ]]
 then
-		echo Extracting ConnectedAlbum Details
+		echo [+] Extracting ConnectedAlbum Details
 		find ../SENSITIVE/private/var/mobile/Library/MediaStream/albumshare/ -name 'Model.sqlite' -exec sqlite3 {} 'SELECT email FROM 'AccessControls'' \; 2>/dev/null | tee ../SENSITIVE/SpiderOUT/ConnectedAlbum.txt
 
-		echo Extracting SharedAlbum URLs
+		echo [+] Extracting SharedAlbum URLs
 		find ../SENSITIVE/private/var/mobile/Library/MediaStream/albumshare/ -name 'Model.sqlite' -exec sqlite3 {} 'SELECT name,url FROM 'Albums'' \; 2>/dev/null | tee ../SENSITIVE/SpiderOUT/SharedAlbum.txt
 fi
 
@@ -88,10 +87,10 @@ if [[ $1 = "-i" ]]
 then
 	if [ $2 = 0 ]
 	then
-		echo "No Information Supplied... Exiting For Safety"
+		echo "[!] No Information Supplied... Exiting For Safety"
 		exit 1
 	fi
-	echo "Searching..."
+	echo "[+] Searching..."
 	find ../SENSITIVE/ -name '*.sqlite' -exec grep -H $2 {} \; 2>/dev/null | tee ../SENSITIVE/SpiderOUT/MATCHSQLITE.txt 2>/dev/null
 	find ../SENSITIVE/ -name '*.db' -exec grep -H $2 {} \; 2>/dev/null | tee ../SENSITIVE/SpiderOUT/MATCHSQLITE.txt 2>/dev/null
 fi
