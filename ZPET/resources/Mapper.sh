39f6cdd630f90554394bf53fe34ae40a00ad57e2
James Duffy <james@duffy.app>
2020-12-02 00:46:41 +0000

    Fixed folder referencing issue w/mapper

--- ZPET/resources/Mapper.sh
+++ ZPET/resources/Mapper.sh
@@ -25,7 +25,7 @@ read -rsn1 -p"Press Enter To Execute...";echo
  echo Removing File Smaller Than 50 Bytes In Build...
  echo ========================================
 
- find ../SENSITIVE/ -type f -size -50c  -exec rm {} \; >/dev/null 2>/dev/null
+ find ./SENSITIVE/ -type f -size -50c  -exec rm {} \; >/dev/null 2>/dev/null
  echo 'Done!'
 
 
@@ -33,20 +33,20 @@ echo ========================================
 echo Finding Databases Larger Than 50KB In Build...
 echo ========================================
 
-find ../SENSITIVE/ -type f -size +50k -name "*.db" -print 2>/dev/null
-find ../SENSITIVE/ -type f -size +50k -name "*.sqlite*" -print 2>/dev/null
-find ../SENSITIVE/ -type f -size +10k -size -800k -not -name "*.db" -not -name "*.sqlite*" -exec file '{}' \; 2>/dev/null | grep -H SQLite | cut -f2 -d':'
+find ./SENSITIVE/ -type f -size +50k -name "*.db" -print 2>/dev/null
+find ./SENSITIVE/ -type f -size +50k -name "*.sqlite*" -print 2>/dev/null
+find ./SENSITIVE/ -type f -size +10k -size -800k -not -name "*.db" -not -name "*.sqlite*" -exec file '{}' \; 2>/dev/null | grep -H SQLite | cut -f2 -d':'
 
 
 echo ========================================
 echo Finding Hidden Media In Build...
 echo ========================================
 
-find ../SENSITIVE/ -type f -size +10k -size -800k -not -name "*.png" -exec file '{}' \; 2>/dev/null | grep -H PNG | cut -f2 -d':' | xargs -I{} open {}
+find ./SENSITIVE/ -type f -size +10k -size -800k -not -name "*.png" -exec file '{}' \; 2>/dev/null | grep -H PNG | cut -f2 -d':' | xargs -I{} open {}
 
-find ../SENSITIVE/ -type f -size +10k -size -800k -not -name "*.jpg" -exec file '{}' \; 2>/dev/null | grep -H JFIF | cut -f2 -d':' | xargs -I{} open {}
+find ./SENSITIVE/ -type f -size +10k -size -800k -not -name "*.jpg" -exec file '{}' \; 2>/dev/null | grep -H JFIF | cut -f2 -d':' | xargs -I{} open {}
 
-find ../SENSITIVE/ -type f -size +80k -not -name "*.wav" -exec file '{}' \; 2>/dev/null | grep -H WAVE | cut -f2 -d':' | xargs -I{} open {}
+find ./SENSITIVE/ -type f -size +80k -not -name "*.wav" -exec file '{}' \; 2>/dev/null | grep -H WAVE | cut -f2 -d':' | xargs -I{} open {}
 
 #
 #find . -type f -size +100k -size -800k -exec grep -H -a Photoshop '{}' \; 2>/dev/null
